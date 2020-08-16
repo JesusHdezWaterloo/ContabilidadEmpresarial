4d97eead9f5f204e7c36ddda401be6a92a73024a
Unknown <unknown@example.com>
2020-08-16 13:47:35 -0500

    CuadreDetailView: +addActionsExtra, +getLiquidado

--- src/main/java/com/jhw/gestion/modules/contabilidad/ui/cuadre/CuadreDetailView.java
+++ src/main/java/com/jhw/gestion/modules/contabilidad/ui/cuadre/CuadreDetailView.java
@@ -2,14 +2,24 @@ package com.jhw.gestion.modules.contabilidad.ui.cuadre;
 
 import com.jhw.gestion.modules.contabilidad.ui.liquidacion.*;
 import com.clean.core.app.services.ExceptionHandler;
+import com.clean.core.app.services.Notification;
+import com.clean.core.app.services.NotificationsGeneralType;
 import com.jhw.gestion.modules.contabilidad.core.domain.*;
 import com.jhw.gestion.modules.contabilidad.ui.module.ContabilidadSwingModule;
+import com.jhw.swing.material.components.button._MaterialButtonIconTransparent;
+import com.jhw.swing.material.components.container.panel._PanelGradient;
 import com.jhw.swing.material.components.table.Column;
+import com.jhw.swing.material.components.table.editors_renders.component.ComponentCellEditor;
+import com.jhw.swing.material.components.table.editors_renders.component.ComponentCellRender;
 import com.jhw.swing.material.components.table.editors_renders.money.MoneyCellRender;
 import com.jhw.swing.material.components.table.editors_renders.money.MoneyTableComponent;
+import com.jhw.swing.material.standards.MaterialColors;
+import com.jhw.swing.material.standards.MaterialIcons;
 import com.jhw.swing.models.input.dialogs.DialogModelInput;
 import com.jhw.swing.models.detail._MaterialPanelDetail;
 import com.jhw.utils.others.SDF;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
 
 /**
  *
@@ -17,6 +27,7 @@ import com.jhw.utils.others.SDF;
  */
 public class CuadreDetailView extends _MaterialPanelDetail<CuadreDomain> {
 
+    private static final String COL_LIQUIDADO = "Liq.";
     private static final String COL_DCUMENTO = "Documento";
     private static final String COL_NOMBRE = "Nombre";
     private static final String COL_FECHA = "Fecha";
@@ -29,6 +40,7 @@ public class CuadreDetailView extends _MaterialPanelDetail<CuadreDomain> {
 
     public CuadreDetailView() {
         super(
+                Column.builder().name(COL_LIQUIDADO).width(5).build(),
                 Column.builder().name(COL_DCUMENTO).build(),
                 Column.builder().name(COL_NOMBRE).build(),
                 Column.builder().name(COL_FECHA).build(),
@@ -45,8 +57,11 @@ public class CuadreDetailView extends _MaterialPanelDetail<CuadreDomain> {
 
     private void personalize() {
         setUpEditorsRenders();
+
+        //addOptionsElements();
+        addActionsExtra();
+
         this.setHeaderText("Operaciones - Cuadre");
-        this.setOptionPanelVisibility(false);
         this.setActionColumnButtonsVisivility(false, false, false);//no pone el view, no esta implementado todavia
     }
 
@@ -61,6 +76,7 @@ public class CuadreDetailView extends _MaterialPanelDetail<CuadreDomain> {
     @Override
     public Object[] getRowObject(CuadreDomain obj) {
         return new Object[]{
+            getLiquidado(obj),
             obj.info().getDocumento(),
             obj.info().getNombre(),
             SDF.format(obj.info().getFecha()),
@@ -71,6 +87,17 @@ public class CuadreDetailView extends _MaterialPanelDetail<CuadreDomain> {
             MoneyTableComponent.from(obj.getOperacionContableCuadreFk().getDebito(), obj.getOperacionContableCuadreFk().getCuentaFk().getMonedaFk()),
             MoneyTableComponent.from(obj.getOperacionContableCuadreFk().getCredito(), obj.getOperacionContableCuadreFk().getCuentaFk().getMonedaFk())
         };
+    }
+
+    private JPanel getLiquidado(CuadreDomain cuadre) {
+        _PanelGradient panel = new _PanelGradient();
+        if (cuadre.getLiquidada()) {
+            panel.setIcon(MaterialIcons.DONE);
+        } else {
+            panel.setBackground(MaterialColors.TRANSPARENT);
+            panel.setIcon(null);
+        }
+        return panel;
     }
 
     @Override
@@ -98,9 +125,50 @@ public class CuadreDetailView extends _MaterialPanelDetail<CuadreDomain> {
     }
 
     private void setUpEditorsRenders() {
+        ComponentCellRender colorRender = new ComponentCellRender(false);
+        getTable().getColumn(COL_LIQUIDADO).setCellEditor(new ComponentCellEditor(colorRender, false));
+        getTable().getColumn(COL_LIQUIDADO).setCellRenderer(colorRender);
+
         getTable().getColumn(COL_DEBITO1).setCellRenderer(new MoneyCellRender());
         getTable().getColumn(COL_CREDITO1).setCellRenderer(new MoneyCellRender());
         getTable().getColumn(COL_DEBITO2).setCellRenderer(new MoneyCellRender());
         getTable().getColumn(COL_CREDITO2).setCellRenderer(new MoneyCellRender());
     }
+
+    private void addActionsExtra() {
+        _MaterialButtonIconTransparent btnLiquidar = new _MaterialButtonIconTransparent();
+        btnLiquidar.setIcon(MaterialIcons.ASSIGNMENT_TURNED_IN.deriveIcon(18f));
+        btnLiquidar.addActionListener((ActionEvent e) -> {
+            onLiquidarMaterialActionPerformed();
+        });
+        this.addActionExtra(btnLiquidar);
+    }
+
+    private void onLiquidarMaterialActionPerformed() {
+        try {
+            CuadreDomain obj = getSelectedElement();
+            if (obj.getLiquidada()) {
+                Notification.showConfirmDialog(NotificationsGeneralType.CONFIRM_INFO, "Ya este cuadre se liquidó.");
+            } else {
+                LiquidacionDomain liq = ContabilidadSwingModule.liquicadionUC.getLiquidacion(obj);
+                new DialogModelInput(this, LiquidacionInputView.fromBase(liq));
+            }
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e);
+        }
+    }
+
+    /*private void addOptionsElements() {
+        //detalles de todas las operaciones
+        _MaterialButtonIconTransparent btnLiquidarAll = new _MaterialButtonIconTransparent();
+        btnLiquidarAll.setIcon(MaterialIcons.VISIBILITY.deriveIcon(30f));
+        btnLiquidarAll.addActionListener((ActionEvent e) -> {
+            onLiquidarAllMaterialActionPerformed();
+        });
+        this.addOptionElement(btnLiquidarAll);
+    }
+
+    private void onLiquidarAllMaterialActionPerformed() {
+        System.out.println("Liquidar todo");
+    }*/
 }

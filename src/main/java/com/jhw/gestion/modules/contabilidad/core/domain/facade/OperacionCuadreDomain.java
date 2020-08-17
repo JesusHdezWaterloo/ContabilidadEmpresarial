b4aed24d859fb598ecd40bb2a1d9383fee3a157f
Unknown <unknown@example.com>
2020-08-17 18:37:18 -0500

    +OperacionCuadreDomain

---
+++ src/main/java/com/jhw/gestion/modules/contabilidad/core/domain/facade/OperacionCuadreDomain.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jhw.gestion.modules.contabilidad.core.domain.facade;
+
+import com.clean.core.domain.VolatileDomainObject;
+import com.jhw.gestion.modules.contabilidad.core.domain.CuadreDomain;
+import com.jhw.gestion.modules.contabilidad.core.domain.CuentaContableDomain;
+import com.jhw.gestion.modules.contabilidad.core.domain.MonedaDomain;
+
+/**
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ */
+public class OperacionCuadreDomain extends VolatileDomainObject {
+
+    private double valor;
+    private MonedaDomain moneda;
+    private CuentaContableDomain cuenta;
+    private CuentaContableDomain cuadre;
+
+    public OperacionCuadreDomain() {
+    }
+
+    public OperacionCuadreDomain(double valor, MonedaDomain moneda, CuentaContableDomain cuenta, CuentaContableDomain cuadre) {
+        this.valor = valor;
+        this.moneda = moneda;
+        this.cuenta = cuenta;
+        this.cuadre = cuadre;
+    }
+
+    public OperacionCuadreDomain(CuadreDomain cuadre) {
+        this.valor = cuadre.getOperacionContableFk().getCuentaFk().getTipoCuentaFk().getDebitoCredito() ? cuadre.getOperacionContableFk().getDebito() : cuadre.getOperacionContableFk().getCredito();
+        this.moneda = cuadre.getOperacionContableFk().getCuentaFk().getMonedaFk();
+        this.cuenta = cuadre.getOperacionContableFk().getCuentaFk();
+        this.cuadre = cuadre.getOperacionContableCuadreFk().getCuentaFk();
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public MonedaDomain getMoneda() {
+        return moneda;
+    }
+
+    public void setMoneda(MonedaDomain moneda) {
+        this.moneda = moneda;
+    }
+
+    public CuentaContableDomain getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(CuentaContableDomain cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public CuentaContableDomain getCuadre() {
+        return cuadre;
+    }
+
+    public void setCuadre(CuentaContableDomain cuadre) {
+        this.cuadre = cuadre;
+    }
+}

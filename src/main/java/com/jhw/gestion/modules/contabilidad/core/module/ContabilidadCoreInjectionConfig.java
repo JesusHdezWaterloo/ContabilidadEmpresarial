4149db1288c7afe0c4ec40035e2e596a0b3eb067
Unknown <unknown@example.com>
2020-08-19 18:17:21 -0500

    Singleton a los UC_IMPL y REPO_IMPL

--- src/main/java/com/jhw/gestion/modules/contabilidad/core/module/ContabilidadCoreInjectionConfig.java
+++ src/main/java/com/jhw/gestion/modules/contabilidad/core/module/ContabilidadCoreInjectionConfig.java
@@ -1,6 +1,7 @@
 package com.jhw.gestion.modules.contabilidad.core.module;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 
 import com.jhw.gestion.modules.contabilidad.core.usecase_def.*;
 import com.jhw.gestion.modules.contabilidad.core.usecase_impl.*;
@@ -14,18 +15,18 @@ public class ContabilidadCoreInjectionConfig extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(CuadreUseCase.class).to(CuadreUseCaseImpl.class);
-        bind(CuentaBancariaUseCase.class).to(CuentaBancariaUseCaseImpl.class);
-        bind(CuentaContableUseCase.class).to(CuentaContableUseCaseImpl.class);
-        bind(InfoOperacionContableUseCase.class).to(InfoOperacionContableUseCaseImpl.class);
-        bind(LiquidacionUseCase.class).to(LiquidacionUseCaseImpl.class);
-        bind(FormaPagoUseCase.class).to(FormaPagoUseCaseImpl.class);
-        bind(MonedaUseCase.class).to(MonedaUseCaseImpl.class);
-        bind(OperacionContableUseCase.class).to(OperacionContableUseCaseImpl.class);
-        bind(SubcuentaUseCase.class).to(SubcuentaUseCaseImpl.class);
-        bind(TipoCuentaUseCase.class).to(TipoCuentaUseCaseImpl.class);
-        bind(TipoOperacionContableUseCase.class).to(TipoOperacionContableUseCaseImpl.class);
-        bind(TitularUseCase.class).to(TitularUseCaseImpl.class);
+        bind(CuadreUseCase.class).to(CuadreUseCaseImpl.class).in(Singleton.class);
+        bind(CuentaBancariaUseCase.class).to(CuentaBancariaUseCaseImpl.class).in(Singleton.class);
+        bind(CuentaContableUseCase.class).to(CuentaContableUseCaseImpl.class).in(Singleton.class);
+        bind(InfoOperacionContableUseCase.class).to(InfoOperacionContableUseCaseImpl.class).in(Singleton.class);
+        bind(LiquidacionUseCase.class).to(LiquidacionUseCaseImpl.class).in(Singleton.class);
+        bind(FormaPagoUseCase.class).to(FormaPagoUseCaseImpl.class).in(Singleton.class);
+        bind(MonedaUseCase.class).to(MonedaUseCaseImpl.class).in(Singleton.class);
+        bind(OperacionContableUseCase.class).to(OperacionContableUseCaseImpl.class).in(Singleton.class);
+        bind(SubcuentaUseCase.class).to(SubcuentaUseCaseImpl.class).in(Singleton.class);
+        bind(TipoCuentaUseCase.class).to(TipoCuentaUseCaseImpl.class).in(Singleton.class);
+        bind(TipoOperacionContableUseCase.class).to(TipoOperacionContableUseCaseImpl.class).in(Singleton.class);
+        bind(TitularUseCase.class).to(TitularUseCaseImpl.class).in(Singleton.class);
     }
 
 }
--- src/main/java/com/jhw/gestion/modules/contabilidad/repo/module/ContabilidadRepoInjectionConfig.java
+++ src/main/java/com/jhw/gestion/modules/contabilidad/repo/module/ContabilidadRepoInjectionConfig.java
@@ -1,6 +1,7 @@
 package com.jhw.gestion.modules.contabilidad.repo.module;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 import com.jhw.gestion.modules.contabilidad.core.repo_def.*;
 import com.jhw.gestion.modules.contabilidad.repo.repo_impl.*;
 
@@ -13,18 +14,18 @@ public class ContabilidadRepoInjectionConfig extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(CuadreRepo.class).to(CuadreRepoImpl.class);
-        bind(CuentaBancariaRepo.class).to(CuentaBancariaRepoImpl.class);
-        bind(CuentaContableRepo.class).to(CuentaContableRepoImpl.class);
-        bind(InfoOperacionContableRepo.class).to(InfoOperacionContableRepoImpl.class);
-        bind(LiquidacionRepo.class).to(LiquidacionRepoImpl.class);
-        bind(FormaPagoRepo.class).to(FormaPagoRepoImpl.class);
-        bind(MonedaRepo.class).to(MonedaRepoImpl.class);
-        bind(OperacionContableRepo.class).to(OperacionContableRepoImpl.class);
-        bind(SubcuentaRepo.class).to(SubcuentaRepoImpl.class);
-        bind(TipoCuentaRepo.class).to(TipoCuentaRepoImpl.class);
-        bind(TipoOperacionContableRepo.class).to(TipoOperacionContableRepoImpl.class);
-        bind(TitularRepo.class).to(TitularRepoImpl.class);
+        bind(CuadreRepo.class).to(CuadreRepoImpl.class).in(Singleton.class);
+        bind(CuentaBancariaRepo.class).to(CuentaBancariaRepoImpl.class).in(Singleton.class);
+        bind(CuentaContableRepo.class).to(CuentaContableRepoImpl.class).in(Singleton.class);
+        bind(InfoOperacionContableRepo.class).to(InfoOperacionContableRepoImpl.class).in(Singleton.class);
+        bind(LiquidacionRepo.class).to(LiquidacionRepoImpl.class).in(Singleton.class);
+        bind(FormaPagoRepo.class).to(FormaPagoRepoImpl.class).in(Singleton.class);
+        bind(MonedaRepo.class).to(MonedaRepoImpl.class).in(Singleton.class);
+        bind(OperacionContableRepo.class).to(OperacionContableRepoImpl.class).in(Singleton.class);
+        bind(SubcuentaRepo.class).to(SubcuentaRepoImpl.class).in(Singleton.class);
+        bind(TipoCuentaRepo.class).to(TipoCuentaRepoImpl.class).in(Singleton.class);
+        bind(TipoOperacionContableRepo.class).to(TipoOperacionContableRepoImpl.class).in(Singleton.class);
+        bind(TitularRepo.class).to(TitularRepoImpl.class).in(Singleton.class);
     }
 
 }

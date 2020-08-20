325dd1e5c55e8ae8717f13d8328db15abd1f1741
Unknown <unknown@example.com>
2020-08-19 21:28:51 -0500

    CuentaContableUseCaseImpl: +checkIntegrity

--- src/main/java/com/jhw/gestion/modules/contabilidad/core/usecase_impl/CuentaContableUseCaseImpl.java
+++ src/main/java/com/jhw/gestion/modules/contabilidad/core/usecase_impl/CuentaContableUseCaseImpl.java
@@ -1,12 +1,16 @@
 package com.jhw.gestion.modules.contabilidad.core.usecase_impl;
 
+import com.clean.core.app.services.Notification;
+import com.clean.core.app.services.NotificationsGeneralType;
 import com.clean.core.app.usecase.DefaultCRUDUseCase;
+import com.clean.core.domain.services.Resource;
 import com.clean.core.exceptions.ValidationException;
 import com.jhw.gestion.modules.contabilidad.core.domain.*;
 import com.jhw.gestion.modules.contabilidad.core.module.ContabilidadCoreModule;
 import com.jhw.gestion.modules.contabilidad.core.usecase_def.*;
 import com.jhw.gestion.modules.contabilidad.core.repo_def.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class CuentaContableUseCaseImpl extends DefaultCRUDUseCase<CuentaContableDomain> implements CuentaContableUseCase {
@@ -15,6 +19,7 @@ public class CuentaContableUseCaseImpl extends DefaultCRUDUseCase<CuentaContable
 
     public CuentaContableUseCaseImpl() {
         super.setRepo(repo);
+        checkIntegrity();
     }
 
     @Override
@@ -23,7 +28,7 @@ public class CuentaContableUseCaseImpl extends DefaultCRUDUseCase<CuentaContable
         if (old.getDebito() != objectToUpdate.getDebito()) {
             throw new ValidationException("debito", "No se puede modificar el débito directamente, solo mediante operaciones.\nProbablemente alguien haya modificado la cuenta externamente.");
         }
-        if (old.getCredito()!= objectToUpdate.getCredito()) {
+        if (old.getCredito() != objectToUpdate.getCredito()) {
             throw new ValidationException("credito", "No se puede modificar el crédito directamente, solo mediante operaciones.\nProbablemente alguien haya modificado la cuenta externamente.");
         }
         if (!old.getMonedaFk().equals(objectToUpdate.getMonedaFk())) {
@@ -69,4 +74,37 @@ public class CuentaContableUseCaseImpl extends DefaultCRUDUseCase<CuentaContable
         return cuentas;
     }
 
+    private void checkIntegrity() {
+        try {
+            HashMap<Integer, CuentaContableDomain> h = new HashMap<>();
+            for (CuentaContableDomain c : super.findAll()) {
+                c.setDebito(0);
+                c.setCredito(0);
+                h.put(c.getIdCuentaContable(), c);
+            }
+
+            CuadreUseCase cuadreUC = ContabilidadCoreModule.getInstance().getImplementation(CuadreUseCase.class);
+            for (CuadreDomain c : cuadreUC.findAll()) {
+                try {
+                    c.validate();
+
+                    CuentaContableDomain ctaMap = h.get(c.getOperacionContableFk().getCuentaFk().getIdCuentaContable());
+                    ctaMap.setDebito(ctaMap.getDebito() + c.getOperacionContableFk().getDebito());
+                    ctaMap.setCredito(ctaMap.getCredito() + c.getOperacionContableFk().getCredito());
+
+                    if (!c.getLiquidada()) {
+                        CuentaContableDomain ctaMapCuadre = h.get(c.getOperacionContableCuadreFk().getCuentaFk().getIdCuentaContable());
+                        ctaMapCuadre.setDebito(ctaMapCuadre.getDebito() + c.getOperacionContableCuadreFk().getDebito());
+                        ctaMapCuadre.setCredito(ctaMapCuadre.getCredito() + c.getOperacionContableCuadreFk().getCredito());
+                    }
+                } catch (Exception e) {
+                }
+            }
+            for (CuentaContableDomain value : h.values()) {
+                repo.edit(value);
+            }
+        } catch (Exception e) {
+            Notification.showConfirmDialog(NotificationsGeneralType.CONFIRM_ERROR, Resource.getString("msg.default_config.error.check_integrity"));
+        }
+    }
 }

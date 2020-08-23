6fa5776a4196c742301cde68beccb2742341c745
Unknown <unknown@example.com>
2020-08-22 22:38:04 -0500

    TipoCuentaInputView: 2 check box para debito-credito

--- src/main/java/com/jhw/gestion/modules/contabilidad/ui/tipo_cuenta/TipoCuentaInputView.java
+++ src/main/java/com/jhw/gestion/modules/contabilidad/ui/tipo_cuenta/TipoCuentaInputView.java
@@ -1,9 +1,12 @@
 package com.jhw.gestion.modules.contabilidad.ui.tipo_cuenta;
 
+import com.clean.core.exceptions.ValidationException;
 import com.jhw.swing.material.components.container.layout.VerticalLayoutContainer;
 import com.jhw.swing.models.clean.CleanCRUDInputView;
 import com.jhw.gestion.modules.contabilidad.core.domain.*;
 import com.jhw.gestion.modules.contabilidad.ui.module.ContabilidadSwingModule;
+import com.jhw.swing.material.components.container.layout.HorizontalLayoutContainer;
+import com.jhw.swing.material.components.toggle.ToggleGroup;
 import com.jhw.swing.material.components.toggle._MaterialCheckBox;
 import java.util.Map;
 
@@ -33,7 +36,16 @@ public class TipoCuentaInputView extends CleanCRUDInputView<TipoCuentaDomain> {
 
         //debito
         checkBoxDebito = new _MaterialCheckBox();
-        checkBoxDebito.setText("Débito (Deseleccionado: Crédito)");
+        checkBoxDebito.setText("Débito");
+        checkBoxDebito.setSelected(true);
+
+        //credito
+        checkBoxCredito = new _MaterialCheckBox();
+        checkBoxCredito.setText("Crédito");
+
+        ToggleGroup group = new ToggleGroup();
+        group.add(checkBoxDebito);
+        group.add(checkBoxCredito);
 
         //liquidable
         checkBoxLiquidable = new _MaterialCheckBox();
@@ -44,7 +56,12 @@ public class TipoCuentaInputView extends CleanCRUDInputView<TipoCuentaDomain> {
 
         VerticalLayoutContainer.builder vlc = VerticalLayoutContainer.builder();
         vlc.add(textFieldTipo);
-        vlc.add(checkBoxDebito);
+
+        HorizontalLayoutContainer.builder hlcDebCred = HorizontalLayoutContainer.builder();
+        hlcDebCred.add(checkBoxDebito);
+        hlcDebCred.add(checkBoxCredito);
+        vlc.add(hlcDebCred.build());
+        
         vlc.add(checkBoxLiquidable);
 
         vlc.add(textAreaDescripcion, true);
@@ -54,15 +71,34 @@ public class TipoCuentaInputView extends CleanCRUDInputView<TipoCuentaDomain> {
     // Variables declaration - do not modify
     private com.jhw.swing.material.components.textfield.validated._MaterialTextFieldStringNotEmpty textFieldTipo;
     private _MaterialCheckBox checkBoxDebito;
+    private _MaterialCheckBox checkBoxCredito;
     private _MaterialCheckBox checkBoxLiquidable;
     private com.jhw.swing.material.components.textarea.prepared._MaterialTextAreaDescripcion textAreaDescripcion;
     // End of variables declaration                   
 
     @Override
+    public void update() {
+        super.update();
+        if (getOldModel() != null) {
+            checkBoxDebito.setSelected(getOldModel().getDebitoCredito());
+            checkBoxCredito.setSelected(!getOldModel().getDebitoCredito());
+        }
+    }
+
+    @Override
+    public TipoCuentaDomain getNewModel() throws Exception {
+        TipoCuentaDomain m = super.getNewModel();
+        if (!checkBoxDebito.isSelected() && !checkBoxCredito.isSelected()) {
+            throw new ValidationException("La cuenta tiene que ser de débito o de crédito");
+        }
+        m.setDebitoCredito(checkBoxDebito.isSelected());
+        return m;
+    }
+
+    @Override
     public Map<String, Object> bindFields() {
         Map<String, Object> bindFields = super.bindFields();
         bindFields.put("nombreTipoCuenta", textFieldTipo);
-        bindFields.put("debitoCredito", checkBoxDebito);
         bindFields.put("liquidable", checkBoxLiquidable);
         bindFields.put("descripcion", textAreaDescripcion);
         return bindFields;

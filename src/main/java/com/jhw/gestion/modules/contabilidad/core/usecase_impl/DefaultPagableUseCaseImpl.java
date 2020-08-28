1846fc3129ca51770aa1747170571c32c0b3cd14
Unknown <unknown@example.com>
2020-08-27 23:47:09 -0500

    +DefaultPagableUseCaseImpl

---
+++ src/main/java/com/jhw/gestion/modules/contabilidad/core/usecase_impl/DefaultPagableUseCaseImpl.java
@@ -0,0 +1,35 @@
+package com.jhw.gestion.modules.contabilidad.core.usecase_impl;
+
+import com.clean.core.app.repo.CRUDRepository;
+import com.clean.core.app.usecase.CRUDUseCase;
+import com.clean.core.app.usecase.DefaultCRUDUseCase;
+import com.jhw.gestion.modules.contabilidad.core.domain.CuadreDomain;
+import com.jhw.gestion.modules.contabilidad.core.domain.Pagable;
+import com.jhw.gestion.modules.contabilidad.core.module.ContabilidadCoreModule;
+import com.jhw.gestion.modules.contabilidad.core.usecase_def.*;
+
+public class DefaultPagableUseCaseImpl<T extends Pagable> extends DefaultCRUDUseCase<T> implements CRUDUseCase<T> {
+
+    private final CuadreUseCase cuadreUC = ContabilidadCoreModule.getInstance().getImplementation(CuadreUseCase.class);
+
+    @Override
+    public T destroy(T objectToDestroy) throws Exception {
+        //destruye el cuadre y por defecto el gasto. Destruye el cuadre viejo xq el nuevo viene con el valor cambiado
+        cuadreUC.destroy(cuadreUC.findBy(objectToDestroy.getCuadreFk().getIdCuadre()));
+        return objectToDestroy;
+    }
+
+    @Override
+    public T edit(T objectToUpdate) throws Exception {
+        destroy(objectToUpdate);
+        return create(objectToUpdate);
+    }
+
+    @Override
+    public T create(T newObject) throws Exception {
+        CuadreDomain cuadre = cuadreUC.create(newObject.getCuadreFk());
+        newObject.setCuadreFk(cuadre);
+        return super.create(newObject);
+    }
+
+}

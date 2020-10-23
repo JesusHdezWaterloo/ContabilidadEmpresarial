832c847950a7bc640b588526c17ec7a3cd765800
Unknown <unknown@example.com>
2020-10-23 13:33:30 -0500

    CuadreRESTService: *path, +PathVariable in findByLiquidada(bln)

--- src/main/java/com/jhw/module/gestion/contabilidad/rest/CuadreRESTService.java
+++ src/main/java/com/jhw/module/gestion/contabilidad/rest/CuadreRESTService.java
@@ -7,7 +7,6 @@ package com.jhw.module.gestion.contabilidad.rest;
 
 import com.jhw.module.gestion.contabilidad.core.ModuleGestionContabilidadEmpresarialConstants;
 import com.jhw.module.gestion.contabilidad.core.domain.CuadreDomain;
-import com.jhw.module.gestion.contabilidad.core.module.ContabilidadCoreModule;
 import com.jhw.module.gestion.contabilidad.core.usecase_def.CuadreUseCase;
 import com.jhw.utils.spring.server.*;
 import java.util.List;
@@ -18,30 +17,30 @@ import org.springframework.web.bind.annotation.*;
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
 @RestController
-@RequestMapping(value = ModuleGestionContabilidadEmpresarialConstants.PATH_CUADRE_GENERAL)
+@RequestMapping(value = ModuleGestionContabilidadEmpresarialConstants.CUADRE_GENERAL_PATH)
 public class CuadreRESTService extends RESTServiceTemplate<CuadreDomain> implements CuadreUseCase {
 
-    private final CuadreUseCase cuadreUC = ContabilidadCoreModule.getInstance().getImplementation(CuadreUseCase.class);
+    private final CuadreUseCase cuadreUC = A_ModuleGestionContabilidadEmpresarial.cuadreUC;
 
     public CuadreRESTService() {
         setUseCase(cuadreUC);
     }
 
     @Override
-    @GetMapping(ModuleGestionContabilidadEmpresarialConstants.PATH_FIND_ALL_PENDING)
+    @GetMapping(ModuleGestionContabilidadEmpresarialConstants.CUADRE_FIND_ALL_PENDING_PATH)
     public List<CuadreDomain> findAllPending() throws Exception {
         return cuadreUC.findAllPending();
     }
 
     @Override
-    @GetMapping(ModuleGestionContabilidadEmpresarialConstants.PATH_FIND_ALL_LIQUIDADAS)
+    @GetMapping(ModuleGestionContabilidadEmpresarialConstants.CUADRE_FIND_ALL_LIQUIDADAS_PATH)
     public List<CuadreDomain> findAllLiquidadas() throws Exception {
         return cuadreUC.findAllLiquidadas();
     }
 
     @Override
-    @GetMapping(ModuleGestionContabilidadEmpresarialConstants.PATH_FIND_BY_LIQUIDADA)
-    public List<CuadreDomain> findByLiquidada(boolean bln) throws Exception {
+    @GetMapping(ModuleGestionContabilidadEmpresarialConstants.CUADRE_FIND_BY_LIQUIDADA_PATH)
+    public List<CuadreDomain> findByLiquidada(@PathVariable(ModuleGestionContabilidadEmpresarialConstants.LIQUIDADA) boolean bln) throws Exception {
         return cuadreUC.findByLiquidada(bln);
     }
 

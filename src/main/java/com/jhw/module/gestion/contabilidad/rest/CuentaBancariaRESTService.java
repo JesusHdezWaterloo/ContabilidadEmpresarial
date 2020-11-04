b8e29a1e8343a9d375daca254d15ec0b45b60110
Unknown <unknown@example.com>
2020-11-04 14:12:34 -0500

    CuentaBancariaRESTService: fix all

--- src/main/java/com/jhw/module/gestion/contabilidad/rest/CuentaBancariaRESTService.java
+++ src/main/java/com/jhw/module/gestion/contabilidad/rest/CuentaBancariaRESTService.java
@@ -5,7 +5,7 @@
  */
 package com.jhw.module.gestion.contabilidad.rest;
 
-import com.jhw.module.gestion.contabilidad.core.ModuleGestionContabilidadEmpresarialConstants;
+import static com.jhw.module.gestion.contabilidad.core.ModuleGestionContabilidadEmpresarialConstants.*;
 import com.jhw.module.gestion.contabilidad.core.domain.*;
 import com.jhw.module.gestion.contabilidad.core.usecase_def.*;
 import com.jhw.utils.spring.server.*;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
 @RestController
-@RequestMapping(value = ModuleGestionContabilidadEmpresarialConstants.CUADRE_GENERAL_PATH)
+@RequestMapping(value = CUENTA_BANCARIA_GENERAL_PATH)
 public class CuentaBancariaRESTService extends RESTServiceTemplate<CuentaBancariaDomain> implements CuentaBancariaUseCase {
 
     private final CuentaBancariaUseCase cuentaBancariaUC = A_ModuleGestionContabilidadEmpresarial.cuentaBancariaUC;
@@ -25,26 +25,23 @@ public class CuentaBancariaRESTService extends RESTServiceTemplate<CuentaBancari
     public CuentaBancariaRESTService() {
         setUseCase(cuentaBancariaUC);
     }
-/*
+
     @Override
-    @GetMapping(ModuleGestionContabilidadEmpresarialConstants.CUADRE_FIND_BY_LIQUIDADA_PATH)
-    public List<CuadreDomain> findByLiquidada(boolean bln) throws Exception {
-        return cuadreUC.findByLiquidada(bln);
-    }
-*/
-    @Override
+    @GetMapping(CUENTA_BANCARIA_FIND_ALL_CUENTAS_PATH)
     public List<Cuenta> findAllCuentas() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return cuentaBancariaUC.findAllCuentas();
     }
 
     @Override
-    public CuentaBancariaDomain findCuentaDefault(MonedaDomain moneda) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @GetMapping(CUENTA_BANCARIA_FIND_DEFAULT_PATH)
+    public CuentaBancariaDomain findCuentaDefault(@PathVariable(LIQUIDADA) MonedaDomain moneda) throws Exception {
+        return cuentaBancariaUC.findCuentaDefault(moneda);
     }
 
     @Override
-    public List<CuentaBancariaDomain> findAll(String searchText) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @GetMapping(CUENTA_BANCARIA_FIND_ALL_SEARCH_PATH)
+    public List<CuentaBancariaDomain> findAll(@PathVariable(SEARCH_TEXT) String searchText) throws Exception {
+        return cuentaBancariaUC.findAll(searchText);
     }
 
 }

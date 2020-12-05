dc60964d3e7d86b5758d7623c9c5973d56f41dc3
Unknown <unknown@example.com>
2020-12-05 11:30:48 -0500

    CoreModule: +init() vacío

--- src/main/java/com/jhw/module/gestion/contabilidad/core/module/ContabilidadCoreModule.java
+++ src/main/java/com/jhw/module/gestion/contabilidad/core/module/ContabilidadCoreModule.java
@@ -4,6 +4,7 @@ import com.clean.core.app.modules.AbstractModule;
 import com.clean.core.app.modules.DefaultAbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.jhw.module.gestion.contabilidad.repo.module.ContabilidadRepoModule;
 
 /**
  * Modulo de contabilidad-core.
@@ -23,6 +24,23 @@ public class ContabilidadCoreModule extends DefaultAbstractModule {
         return INSTANCE;
     }
 
+    public static ContabilidadCoreModule init() {
+        if (INSTANCE != null) {
+            return INSTANCE;
+        }
+        INSTANCE = new ContabilidadCoreModule();
+        INSTANCE.registerModule(ContabilidadRepoModule.init());
+        return getInstance();
+    }
+
+    /**
+     * Usar init() sin repo por parametro para usar el repo por defecto
+     *
+     * @param repoModule
+     * @return
+     * @deprecated
+     */
+    @Deprecated
     public static ContabilidadCoreModule init(AbstractModule repoModule) {
         INSTANCE = new ContabilidadCoreModule();
         INSTANCE.registerModule(repoModule);

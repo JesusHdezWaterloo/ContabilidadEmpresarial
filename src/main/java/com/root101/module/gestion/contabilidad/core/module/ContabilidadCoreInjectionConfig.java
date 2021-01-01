e2f656b6ca8043aaa26b1c660a7418bae8032748
Unknown <unknown@example.com>
2020-12-31 21:49:12 -0500

    Licence a las clases

--- src/main/java/com/root101/module/gestion/contabilidad/core/module/ContabilidadCoreInjectionConfig.java
+++ src/main/java/com/root101/module/gestion/contabilidad/core/module/ContabilidadCoreInjectionConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.root101.module.gestion.contabilidad.core.module;
 
 import com.google.inject.Singleton;
@@ -6,16 +22,16 @@ import com.root101.module.gestion.contabilidad.core.usecase_impl.*;
 import com.jhw.module.util.licence.core.injection.LicenceInjectionConfig;
 
 /**
- * Configuracion del injection del modulo de contabilidad-core.
  *
- * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
  */
 public class ContabilidadCoreInjectionConfig extends LicenceInjectionConfig {
 
     @Override
     protected void configure() {
         super.configure();//configura la licencia
-        
+
         bind(CuadreUseCase.class).to(CuadreUseCaseImpl.class).in(Singleton.class);
         bind(CuentaBancariaUseCase.class).to(CuentaBancariaUseCaseImpl.class).in(Singleton.class);
         bind(CuentaContableUseCase.class).to(CuentaContableUseCaseImpl.class).in(Singleton.class);
